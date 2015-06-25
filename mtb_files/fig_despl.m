8a1ecaa3130f639c658bd70527971859d0e1e83e
Julio Waissman Vilanova <juliowaissman@gmail.com>
2015-06-25 16:20:37 -0700

    script para graficar los puntos de impacto

---
+++ mtb_files/fig_despl.m
@@ -0,0 +1,23 @@
+%% Genera la figura de desplazamientos con compensación y sin compensación
+% Para el caso general con todos los errores en 2.5 mrad
+
+archivo_posiciones = '../campos_info/posiciones_grande.txt';
+archivos_datos = '../data_gen/campo_grande/heliostato';    
+des_str = sprintf('_%1.2f', [2.5e-3, 2.5e-3, 2.5e-3, 2.5e-3, 0] * 1e3);
+
+heliostatos = load(archivo_posiciones, '-ascii');  
+
+for i = randsample(1:length(heliostatos), 15)
+    load([archivos_datos, int2str(i), des_str, '.mat']);
+    
+    figure(1)
+    plot(desplaz(DT(:,1) == 80, 1), desplaz(DT(:,1) == 80, 1), 'k');
+    hold on;
+    
+    figure(2)
+    plot(desplaz(DT(:,1) == 80, 1), desplaz(DT(:,1) == 80, 1), 'k');
+    hold on;
+
+    disp(heliostatos(1,:));
+end
+
